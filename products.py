9d82aafecf5cd24277a39c332ab51dc45d98c97c
Oscar <oscar6292@gmail.com>
2019-03-03 18:37:09 +0800

    add products2.py

--- products.py
+++ products.py
@@ -5,7 +5,18 @@ while True:
 		print('結束')
 		break
 	price = input('請輸入商品價格: ')
-	products.append([name, price])
+	price = int(price)
+	p = [name, price]
+	products.append(p)
 print (products)
 
-products [0][0]
+for p in products:
+    print(p[0], '的價格是', p[1])
+
+with open('prodicts.csv', 'w', encoding='utf-8')as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n') #加法只能用於字串加字串，若型別是int會變數字加法
+
+
+
